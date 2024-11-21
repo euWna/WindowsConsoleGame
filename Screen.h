057d47823b0825f875fa7f892bf28c4cc62c6a46
cse <cse@DESKTOP-2HN27IT>
2024-11-22 04:20:09 +0900

    초기 커밋.

---
+++ Screen.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "sample/Console_sample.h"
+
+void sc_Title();
---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <memory.h>
+#include <Windows.h>
+#include "sample/Console_sample.h"
+#include "Screen.h"
+
+char szScreenBuffer[dfSCREEN_HEIGHT][dfSCREEN_WIDTH];
+
+int iPlayerPosX = dfSCREEN_WIDTH/2;
+int iPlayerPosY = dfSCREEN_HEIGHT/2;
+
+
+int main()
+{
+	cs_Initial();
+	sc_Title();
+}
